337add6d3e1b4409dd5cffd2bbe816976f6ad2da
Rudra Hatte <116587565+Rudra-Hatte@users.noreply.github.com>
2024-08-06 15:46:26 +0530

    Add files via upload

---
+++ Yash.py
@@ -0,0 +1,19 @@
+def main():
+n = int(input("Enter the number of elements "))
+a = []
+for i in range(n):
+    a.append(n)
+
+hash_table = {}
+for j in a :
+    if j in hash_table:
+        hash_table[j]+=1
+    else:
+        hash_table[j] = 1
+    
+q = int(input("Enter the number of queries  "))
+
+for k in range(q):
+    num = int(input("Enter the queries "))
+    print(hash_table.get(num,0))
+    
